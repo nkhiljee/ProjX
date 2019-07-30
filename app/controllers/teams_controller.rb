b5e368f38ebfc784e4ad46d271123d0f183d97d3
github username <github email address>
2019-07-30 08:54:12 -0500

    some css

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,15 @@
 class TeamsController < ApplicationController
 
+    
+    def create
+    
+        redirect_to user_path(@team.slug)
+    end
+    
+    
     def show
-        @team = Team.find(params[:id])
+        @team = Team.find_by_slug(params[:slug])        
+        # @team = Team.find(params[:id])
         @post = Post.find(params[:id])
         @user = User.find(params[:id])
     end
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,26 @@ class Team < ApplicationRecord
     has_many :projects
     has_many :users
     has_many :users, through: :userteams
+
+
+    def slug
+        letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+        result_slug = ""
+        self[:username].split("").each do |c|
+            if  letters.include?(c)
+                result_slug = result_slug+c.downcase
+            elsif c == " "
+                result_slug = result_slug+"-"
+            else 
+                result_slug = result_slug+c
+            end
+        end
+        result_slug
+    end
+
+    def self.find_by_slug(s_find)
+        Team.all.find do |team| 
+            team.slug == s_find
+        end
+    end
 end
