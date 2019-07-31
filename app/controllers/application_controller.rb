f4f8029fb164a70682e6e2473e5dce192bd5db69
github username <github email address>
2019-07-30 20:14:58 -0500

    working authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,27 @@
 class ApplicationController < ActionController::Base
+    # skip_before_action :authenticated, only: [:home]
+    before_action :authenticated, except: [:home]
+
 
     def home
         render  '/applications/home.html'
+    end
+
+    def current_user
+        if session[:user_id]
+            @user = User.find(session[:user_id])
+        else
+            #nil
+        end
+        # @user = User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def authenticated 
+        redirect_to login_path unless logged_in?
     end
 
 end
