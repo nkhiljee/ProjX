170e19225771bd954d2364ad8f0cf76441cf6b81
Aaron Briscoe <gmail2008@gmail.com>
2019-08-01 14:16:33 -0500

    minor changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,6 @@ class UsersController < ApplicationController
     def user_params
         params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
     end
-
     
     
 
